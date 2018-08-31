442cbcbb8396a1da1a87c4a3c6098309a37e6f90
Jesse Preiner <jezzah@gmail.com>
2018-08-31 17:01:09 -0600

    trying to prevent keyboard from fucking my life
    "

--- components/Contact.js
+++ components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, TextInput, StyleSheet, Text, Platform } from 'react-native';
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { Card, FormLabel, FormInput, FormValidationMessage, Form, Button } from 'react-native-elements';
 import LogoSpinner from './LogoSpinner';
 
@@ -36,22 +36,23 @@ class ContactForm extends React.Component {
         <View style={{flex: 1, marginTop: 50}}>
           <LogoSpinner width={150} height={150} />
         </View>
-        <View style={{flex: 3}}>
-          <Card title="Contact us">
-            <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>You</FormLabel>
-            <FormInput onChangeText={(val) => this.setState({'name': val})} placeholder="Joe Smith" value={this.name} />
+          <KeyboardAvoidingView style={{flex: 2, marginTop: 50}}>
+            <Card title="Get in touch" titleStyle={{fontSize: 24}} >
+              <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>You</FormLabel>
+              <FormInput onChangeText={(val) => this.setState({'name': val})} placeholder="Joe Smith" value={this.name} />
 
-            <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>Your email address</FormLabel>
-            <FormInput onChangeText={(val) => this.setState({'email': val})} placeholder="s.w.hawking@damtp.cam.ac.uk" value={this.email} />
+              <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>Your email address</FormLabel>
+              <FormInput onChangeText={(val) => this.setState({'email': val})} placeholder="s.w.hawking@damtp.cam.ac.uk" value={this.email} />
 
-            <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>What's up?</FormLabel>
-            <FormInput multiline onChangeText={(val) => this.setState({'messageContent': val})} placeholder="Hi! You should should..." value={this.messageContent} />
+              <FormLabel labelStyle={{fontFamily: Platform.OS === "ios" ? 'HelveticaNeue' : 'sans-serif'}}>What's up?</FormLabel>
+              <FormInput containerStyle={{ }} inputStyle={{height:'30%'}
+              } multiline onChangeText={(val) => this.setState({'messageContent': val})} placeholder="Hi! You should should..." value={this.messageContent} />
 
-            <FormValidationMessage>{this.state.errorMessage}</FormValidationMessage>
+              <FormValidationMessage>{this.state.errorMessage}</FormValidationMessage>
 
-            <Button title="Send" accessibilityLabel="Send your feedback to playsask">Send</Button>
-          </Card>
-        </View>
+              <Button title="Send" accessibilityLabel="Send your feedback to playsask">Send</Button>
+            </Card>
+          </KeyboardAvoidingView>
       </View>
     )
   }
